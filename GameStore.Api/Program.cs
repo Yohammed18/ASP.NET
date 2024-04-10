e788f7d4b76255b9f6c776e02e15d087af5a0f1e
Mohammed Allibalogun <mallibalogun18@gmail.com>
2024-04-10 12:46:41 -0400

    Updated api to return json list of games from the Dto record.

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,19 @@
+//import class
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//declare a list that will hold the data from Dtos
+List<GameDto> gameStores = [
+    new GameDto(1, "Call Of Duty", "Combat", 59.99M, new DateOnly(1995,5,19)),
+    new GameDto(2, "The Witcher 3: Wild Hunt", "Action RPG", 49.99M, new DateOnly(2015,5,19)),
+    new GameDto(3, "Grand Theft Auto V", "Action-adventure", 39.99M, new DateOnly(2013,9,17)),
+    new GameDto(4, "Red Dead Redemption 2", "Action-adventure", 59.99M, new DateOnly(2018,10,26)),
+    new GameDto(5, "The Last of Us Part II", "Action-adventure", 49.99M, new DateOnly(2020,6,19)),
+    new GameDto(6, "Cyberpunk 2077", "Action RPG", 59.99M, new DateOnly(2020,12,10))
+];
+
+app.MapGet("/", () => gameStores);
 
 app.Run();
